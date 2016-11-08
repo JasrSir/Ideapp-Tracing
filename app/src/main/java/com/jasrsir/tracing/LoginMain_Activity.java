e55487df0d2f067c4abf4ba2a1f4571854f8c0b4
Juan Antonio Suarez <jasrsir@gmail.com>
2016-11-08 22:05:06 +0100

    Icons add and selected User Type

--- app/src/main/java/com/jasrsir/tracing/LoginMain_Activity.java
+++ app/src/main/java/com/jasrsir/tracing/LoginMain_Activity.java
@@ -1,7 +1,9 @@
 package com.jasrsir.tracing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginMain_Activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class LoginMain_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+    }
+
+    public void onClickRegister(View view) {
+        Intent intent;
+        if (view.getId() == R.id.txvNoAccount) {
+            intent = new Intent(LoginMain_Activity.this, SelectorUser_Activity.class);
+            startActivity(intent);
+        }
     }
 }
