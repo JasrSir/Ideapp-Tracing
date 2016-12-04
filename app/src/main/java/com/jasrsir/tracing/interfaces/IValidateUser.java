6ba39a92c131a781cc288c055427cbf92e50c93d
Juan Antonio Suarez <jasrsir@gmail.com>
2016-12-04 18:36:49 +0100

    Errors and Interfaces addes not functional

---
+++ app/src/main/java/com/jasrsir/tracing/interfaces/IValidateUser.java
@@ -0,0 +1,20 @@
+package com.jasrsir.tracing.interfaces;
+
+/**
+ * Created by Jasrsir on 04/12/2016.
+ */
+
+public interface IValidateUser {
+
+    interface View{
+        void setMessageError(String messageError, int idView);
+        void startActivity();
+    }
+
+    interface Presenter {
+        int validateCredentialsEmail(String email);
+
+        int validateCredentialsPassword(String password);
+    }
+
+}
